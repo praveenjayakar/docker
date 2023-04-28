6c0dc1f8fd0cdbb8a0d5908fa28d3d093be791a0
praveenjayakar <49543930+praveenjayakar@users.noreply.github.com>
2023-04-28 14:10:53 +0530

    Create kubeadm.sh

---
+++ kubeadm.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+echo "Installing container runtime..."
+
+# Update package index and install required packages
+sudo apt-get update -y
+sudo apt-get install -y \
+    ca-certificates \
+    curl \
+    gnupg \
+    lolcat
+
+# Install Docker's GPG key
+sudo install -m 0755 -d /etc/apt/trusted.gpg.d/
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/docker.gpg
+
+# Add Docker's APT repository
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/trusted.gpg.d/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# Update package index and install Docker
+sudo apt-get update -y
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# Remove containerd's state directory and restart the service
+sudo rm -rf /etc/containerd/*
+sudo systemctl restart containerd
+
+# Check Docker and containerd status
+sudo systemctl status docker | cat
+sudo systemctl status containerd | cat
+
+# Disable swap for Kubernetes
+sudo swapoff -a
+
+# Load kernel modules required by Kubernetes
+cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+overlay
+br_netfilter
+EOF
+sudo modprobe overlay
+sudo modprobe br_netfilter
+
+# Set kernel parameters required by Kubernetes
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-iptables = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+net.ipv4.ip_forward = 1
+EOF
+sudo sysctl --system
+
+# Install Kubernetes packages
+echo "Installing kubeadm, kubelet, and kubectl..."
+sudo apt-get update
+sudo apt-get install -y apt-transport-https ca-certificates curl
+curl -fsSLo /etc/apt/trusted.gpg.d/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+echo "deb [signed-by=/etc/apt/trusted.gpg.d/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+sudo apt-get install -y kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl
+
+echo "Done!"
