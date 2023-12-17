7e2eaaa1227efc2338a2129ff4f8c5d01d3919d7
Praveen jayakar <49543930+praveenjayakar@users.noreply.github.com>
2023-12-17 17:29:39 +0530

    Update kubeadm-22-04.sh

--- kubeadm-22-04.sh
+++ kubeadm-22-04.sh
@@ -1,6 +1,4 @@
-
 #!/bin/bash
-
 echo "Installing container runtime..."
 
 # Update package index and install required packages
@@ -54,23 +52,15 @@ sudo sysctl --system
 echo "Installing kubeadm, kubelet, and kubectl..."
 sudo apt-get update
 sudo apt-get install -y apt-transport-https ca-certificates curl
-sleep 2
-sudo mkdir -p /etc/apt/keyrings
 
+sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.29/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 
-sleep 1
-
-
-
 echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.29/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
-
-sleep 3
 
 sudo apt-get update
 sudo apt-get install -y kubectl
 sudo apt-mark hold kubectl
-sleep 2
 
 CNI_PLUGINS_VERSION="v1.3.0"
 ARCH="amd64"
@@ -78,51 +68,38 @@ DEST="/opt/cni/bin"
 sudo mkdir -p "$DEST"
 curl -L "https://github.com/containernetworking/plugins/releases/download/${CNI_PLUGINS_VERSION}/cni-plugins-linux-${ARCH}-${CNI_PLUGINS_VERSION}.tgz" | sudo tar -C "$DEST" -xz
 
-sleep 2
-
-
-
 DOWNLOAD_DIR="/usr/local/bin"
 sudo mkdir -p "$DOWNLOAD_DIR"
-
-sleep 2
-
 
 CRICTL_VERSION="v1.28.0"
 ARCH="amd64"
 curl -L "https://github.com/kubernetes-sigs/cri-tools/releases/download/${CRICTL_VERSION}/crictl-${CRICTL_VERSION}-linux-${ARCH}.tar.gz" | sudo tar -C $DOWNLOAD_DIR -xz
 
-sleep 1
-
 RELEASE="$(curl -sSL https://dl.k8s.io/release/stable.txt)"
 ARCH="amd64"
 cd $DOWNLOAD_DIR
 sudo curl -L --remote-name-all https://dl.k8s.io/release/${RELEASE}/bin/linux/${ARCH}/{kubeadm,kubelet}
-sudo chmod +x {kubeadm,kubelet}
+sudo chmod +x *
 
 RELEASE_VERSION="v0.16.2"
 curl -sSL "https://raw.githubusercontent.com/kubernetes/release/${RELEASE_VERSION}/cmd/krel/templates/latest/kubelet/kubelet.service" | sed "s:/usr/bin:${DOWNLOAD_DIR}:g" | sudo tee /etc/systemd/system/kubelet.service
 sudo mkdir -p /etc/systemd/system/kubelet.service.d
 curl -sSL "https://raw.githubusercontent.com/kubernetes/release/${RELEASE_VERSION}/cmd/krel/templates/latest/kubeadm/10-kubeadm.conf" | sed "s:/usr/bin:${DOWNLOAD_DIR}:g" | sudo tee /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
 
-sleep 2
-
+sudo apt-get install -y kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl
 systemctl enable --now kubelet
 
-
 echo "Done!"
-
-
-#kubeip=$(ip addr | grep -i eth | grep -i inet | awk '{print $2}'| cut -d "/" -f1)
-
+#kubeip=$(ip addr | grep -i eth | grep -i inet | awk '{print $2}'| cut -d
+"/" -f1)
 #sudo kubeadm init --control-plane-endpoint=${kubeip} --pod-network-cidr=192.168.0.0/16 --cri-socket=unix:///var/run/containerd/containerd.sock
 
+#kubeip=$(ip addr | grep -i eth | grep -i inet | awk '{print $2}'| cut -d "/" -f1)
+#sudo kubeadm init --control-plane-endpoint=${kubeip} --pod-network-cidr=192.168.0.0/16 --cri-socket=unix:///var/run/containerd/containerd.sock
 #curl https://raw.githubusercontent.com/projectcalico/calico/v3.26.0/manifests/calico.yaml -O
-
 #kubectl apply -f calico.yaml
-
 #if you want to untaint the node to deploy pods on master node
-
 #use this command kubectl taint nodes localhost node-role.kubernetes.io/control-plane:NoSchedule-
-
 #in the name of localhost give the cluster name .
+
