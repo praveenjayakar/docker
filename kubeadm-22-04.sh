e4e982bba2026386cbc067876d20ec97d43a3823
Praveen jayakar <49543930+praveenjayakar@users.noreply.github.com>
2024-02-04 20:12:09 +0530

    Update kubeadm-22-04.sh
    Updated script for ubuntu 22.04 latest

--- kubeadm-22-04.sh
+++ kubeadm-22-04.sh
@@ -1,108 +1,77 @@
 #!/bin/bash
-echo "Installing container runtime..."
 
-# Update package index and install required packages
-sudo apt-get update -y
-sudo apt-get install -y \
-    ca-certificates \
-    curl \
-    gnupg \
-    lolcat
+set -e
 
-ufw disable
+echo "[Task 0] Updating all the packages"
+sudo apt-get update -qq -y && sudo apt-get upgrade -qq -y
+
+echo "[TASK 1] Disable and turn off SWAP"
+sed -i '/swap/d' /etc/fstab
 swapoff -a
 
-echo "Disabling swap..."
+echo "[TASK 2] Stop and Disable firewall"
+systemctl disable --now ufw >/dev/null 2>&1
 
-# Disable swap for the current session
-sudo swapoff -a
-
-# Comment out the swap entry in /etc/fstab to make it persistent across reboots
-sudo sed -i '/\sswap\s/s/^/#/' /etc/fstab
-
-echo "Swap disabled successfully."
-
-
-# Install Docker's GPG key
-sudo install -m 0755 -d /etc/apt/trusted.gpg.d/
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/docker.gpg
-
-# Add Docker's APT repository
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/trusted.gpg.d/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-
-# Update package index and install Docker
-sudo apt-get update -y
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-
-# Remove containerd's state directory and restart the service
-sudo rm -rf /etc/containerd/*
-sudo systemctl restart containerd
-
-# Check Docker and containerd status
-timeout 3 systemctl status docker
-echo "__///////\\\\\\\__"
-timeout 3 systemctl status containerd
-
-# Disable swap for Kubernetes
-sudo swapoff -a
-
-# Load kernel modules required by Kubernetes
-cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+echo "[TASK 3] Enable and Load Kernel modules"
+cat >>/etc/modules-load.d/containerd.conf <<EOF
 overlay
 br_netfilter
 EOF
-sudo modprobe overlay
-sudo modprobe br_netfilter
+modprobe overlay
+modprobe br_netfilter
 
-# Set kernel parameters required by Kubernetes
-cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
-net.bridge.bridge-nf-call-iptables = 1
+echo "[TASK 4] Add Kernel settings"
+cat >>/etc/sysctl.d/kubernetes.conf <<EOF
 net.bridge.bridge-nf-call-ip6tables = 1
-net.ipv4.ip_forward = 1
+net.bridge.bridge-nf-call-iptables  = 1
+net.ipv4.ip_forward                 = 1
 EOF
-sudo sysctl --system
+sysctl --system >/dev/null 2>&1
 
-# Install Kubernetes packages
-echo "Installing kubeadm, kubelet, and kubectl..."
-sudo apt-get update
-sudo apt-get install -y apt-transport-https ca-certificates curl
+echo "[TASK 5] Install containerd runtime"
+export DEBIAN_FRONTEND=noninteractive
+apt-get update -qq >/dev/null
+apt-get install -qq -y apt-transport-https ca-certificates curl gnupg lsb-release >/dev/null
+mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" > /etc/apt/sources.list.d/docker.list
+apt-get update -qq >/dev/null
+apt-get install -qq -y containerd.io >/dev/null
+containerd config default > /etc/containerd/config.toml
+sed -i 's/SystemdCgroup = false/SystemdCgroup = true/g' /etc/containerd/config.toml
+systemctl restart containerd
+systemctl enable containerd >/dev/null
 
-sudo mkdir -p /etc/apt/keyrings
-curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.29/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo "[TASK 6] Set up Kubernetes repo"
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.29/deb/Release.key | gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.29/deb/ /' > /etc/apt/sources.list.d/kubernetes.list
 
-echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.29/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+echo "[TASK 7] Install Kubernetes components (kubeadm, kubelet and kubectl)"
+apt-get update -qq >/dev/null
+apt-get install -qq -y kubeadm kubelet kubectl >/dev/null
 
+echo "----------"
 
-sudo apt-get update
+kubeip=$(ip addr | grep -i eth | grep -i inet | awk '{print $2}' | cut -d "/" -f1)
 
-DOWNLOAD_DIR="/usr/local/bin"
+sudo kubeadm init --control-plane-endpoint=${kubeip} --pod-network-cidr=192.168.0.0/16 --cri-socket=unix:///var/run/containerd/containerd.sock
 
-RELEASE="$(curl -sSL https://dl.k8s.io/release/stable.txt)"
-ARCH="amd64"
-cd $DOWNLOAD_DIR
-sudo curl -L --remote-name-all https://dl.k8s.io/release/${RELEASE}/bin/linux/${ARCH}/{kubelet}
-sudo chmod +x *
+mkdir -p $HOME/.kube
+sleep 1
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sleep 1
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+sleep 1
+curl https://raw.githubusercontent.com/projectcalico/calico/v3.26.0/manifests/calico.yaml -O
+sleep 1
+kubectl apply -f calico.yaml
+sleep 20
 
-RELEASE_VERSION="v0.16.2"
-curl -sSL "https://raw.githubusercontent.com/kubernetes/release/${RELEASE_VERSION}/cmd/krel/templates/latest/kubelet/kubelet.service" | sed "s:/usr/bin:${DOWNLOAD_DIR}:g" | sudo tee /etc/systemd/system/kubelet.service
-sudo mkdir -p /etc/systemd/system/kubelet.service.d
-curl -sSL "https://raw.githubusercontent.com/kubernetes/release/${RELEASE_VERSION}/cmd/krel/templates/latest/kubeadm/10-kubeadm.conf" | sed "s:/usr/bin:${DOWNLOAD_DIR}:g" | sudo tee /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
+# If you want to untaint the node to deploy pods on the master node
+kubectl taint nodes localhost node-role.kubernetes.io/control-plane:NoSchedule-
 
-sudo apt-get install -y kubelet kubeadm kubectl
-sudo apt-mark hold kubelet kubeadm kubectl
-sudo systemctl enable kubelet
-systemctl enable --now kubelet
+# In the name of localhost, give the cluster name.
+kubectl get nodes
 
-
-echo "Done!"
-#kubeip=$(ip addr | grep -i eth | grep -i inet | awk '{print $2}'| cut -d "/" -f1)
-#sudo kubeadm init --control-plane-endpoint=${kubeip} --pod-network-cidr=192.168.0.0/16 --cri-socket=unix:///var/run/containerd/containerd.sock
-
-#kubeip=$(ip addr | grep -i eth | grep -i inet | awk '{print $2}'| cut -d "/" -f1)
-#sudo kubeadm init --control-plane-endpoint=${kubeip} --pod-network-cidr=192.168.0.0/16 --cri-socket=unix:///var/run/containerd/containerd.sock
-#curl https://raw.githubusercontent.com/projectcalico/calico/v3.26.0/manifests/calico.yaml -O
-#kubectl apply -f calico.yaml
-#if you want to untaint the node to deploy pods on master node
-#use this command kubectl taint nodes localhost node-role.kubernetes.io/control-plane:NoSchedule-
-#in the name of localhost give the cluster name .
-
+echo "Congrats man"
