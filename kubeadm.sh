f0343b304e9bcc57dbddf64caa3fac6df0798bc0
Praveen jayakar <49543930+praveenjayakar@users.noreply.github.com>
2024-04-06 12:16:05 +0530

    corrected the script for fetching name of cluster

--- kubeadm.sh
+++ kubeadm.sh
@@ -95,7 +95,7 @@ source ~/.bashrc
 sleep 20
 
 # Task 11
-kubectl taint nodes $(hostname) node-role.kubernetes.io/control-plane:NoSchedule-
+kubectl taint nodes $(kubectl get nodes | awk '{print $1}'|grep -v NAME) node-role.kubernetes.io/control-plane:NoSchedule-
 
 # Task 12
 kubectl get nodes
