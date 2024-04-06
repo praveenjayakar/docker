97fb9223b7a057debf1a80237aead332d6670918
Praveen jayakar <49543930+praveenjayakar@users.noreply.github.com>
2024-04-06 12:05:08 +0530

    updated with some modifications

--- kubeadm.sh
+++ kubeadm.sh
@@ -55,11 +55,13 @@ sudo apt-get update
 sudo apt-get install -y apt-transport-https ca-certificates curl
 sleep 2
 sudo mkdir -p /etc/apt/keyrings
-curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-archive-keyring.gpg ;
+# curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-archive-keyring.gpg ;
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.29/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg;
 sleep 2
 
 
-echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+# echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.29/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
 sleep 3
 
@@ -70,16 +72,54 @@ sudo apt-mark hold kubelet kubeadm kubectl
 echo "Done!"
 
 
-#kubeip=$(ip addr | grep -i eth | grep -i inet | awk '{print $2}'| cut -d "/" -f1)
+# Task 8
+echo "----------"
+kubeip=$(ip addr | grep -i eth | grep -i inet | awk '{print $2}' | cut -d "/" -f1)
+sudo kubeadm init --control-plane-endpoint=${kubeip} --pod-network-cidr=192.168.0.0/16 --cri-socket=unix:///var/run/containerd/containerd.sock
 
-#sudo kubeadm init --control-plane-endpoint=${kubeip} --pod-network-cidr=192.168.0.0/16 --cri-socket=unix:///var/run/containerd/containerd.sock
+# Task 9
+mkdir -p $HOME/.kube
+sleep 1
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sleep 1
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+sleep 1
 
-#curl https://raw.githubusercontent.com/projectcalico/calico/v3.26.0/manifests/calico.yaml -O
+# Task 10
+wget https://github.com/kubecolor/kubecolor/releases/download/v0.2.2/kubecolor_0.2.2_linux_amd64.tar.gz
+tar -xzvf kubecolor_0.2.2_linux_amd64.tar.gz
+mv kubecolor /usr/local/bin
 
-#kubectl apply -f calico.yaml
+echo "alias k=kubecolor" >> ~/.bashrc
+source ~/.bashrc
+sleep 20
 
-#if you want to untaint the node to deploy pods on master node
+# Task 11
+kubectl taint nodes localhost node-role.kubernetes.io/control-plane:NoSchedule-
 
-#use this command kubectl taint nodes localhost node-role.kubernetes.io/control-plane:NoSchedule-
+# Task 12
+kubectl get nodes
 
-#in the name of localhost give the cluster name .
+# Task 13
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh > /dev/null
+sleep 1
+helm version
+
+# Task 14
+sleep 20
+CILIUM_CLI_VERSION=$(curl -s https://raw.githubusercontent.com/cilium/cilium-cli/main/stable.txt)
+CLI_ARCH=amd64
+if [ "$(uname -m)" = "aarch64" ]; then CLI_ARCH=arm64; fi
+curl -L --fail --remote-name-all https://github.com/cilium/cilium-cli/releases/download/${CILIUM_CLI_VERSION}/cilium-linux-${CLI_ARCH}.tar.gz{,.sha256sum}
+sha256sum --check cilium-linux-${CLI_ARCH}.tar.gz.sha256sum
+sudo tar xzvfC cilium-linux-${CLI_ARCH}.tar.gz /usr/local/bin
+rm cilium-linux-${CLI_ARCH}.tar.gz{,.sha256sum}
+sleep 20
+
+# Task 15
+cilium install --version 1.15.1
+sleep 10
+
+echo "Congrats man"
